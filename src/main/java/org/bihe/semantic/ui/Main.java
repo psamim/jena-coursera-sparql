4c4dc1975bcbe13a33a108171f6238ba0d4610f1
Samim Pezeshki <p.samim@gmail.com>
2015-02-18 22:09:04 +0330

    Add template as constant

--- src/main/java/org/bihe/semantic/ui/Main.java
+++ src/main/java/org/bihe/semantic/ui/Main.java
@@ -7,13 +7,16 @@ import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
 public class Main {
+	// HTML template in src/main/resources/spark/template/freemarker
+	public static final String TEMPLATE = "template.ftl";
+	
 	public static void main(String[] args) {
 		staticFileLocation("/public"); // Static files
 
 		get("/search", (rq, rs) -> {
 			Map<String, Object> attributes = new HashMap<>();
 			attributes.put("page", "search");
-			return new ModelAndView(attributes, "template.ftl");
+			return new ModelAndView(attributes, TEMPLATE);
 		}, new FreeMarkerEngine());
 
 		get("/results", (rq, rs) -> {
@@ -24,8 +27,8 @@ public class Main {
 			search.setCategory(rq.queryParams("category"));
 			search.setName(rq.queryParams("name"));
 
-			return new ModelAndView(attributes, "template.ftl");
+
+			return new ModelAndView(attributes, TEMPLATE);
 		}, new FreeMarkerEngine());
 	}
-
 }
