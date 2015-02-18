85acf7180274879c19e48e8d3bd55a22a9da9fe6
Samim Pezeshki <p.samim@gmail.com>
2015-02-18 16:35:39 +0330

    Create Search object

---
+++ src/main/java/org/bihe/semantic/ui/Search.java
@@ -0,0 +1,19 @@
+package org.bihe.semantic.ui;
+
+public class Search {
+	private String name;
+	private String category;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+}
