4dfb55ac0af6d74b833aab6ded79f7aabd858736
Samim Pezeshki <p.samim@gmail.com>
2015-02-20 18:03:01 +0330

    Add nothing

--- src/main/java/org/bihe/semantic/model/Modeling.java
+++ src/main/java/org/bihe/semantic/model/Modeling.java
@@ -1,11 +1,12 @@
 package org.bihe.semantic.model;
+
 import com.hp.hpl.jena.graph.NodeFactory;
 import com.hp.hpl.jena.rdf.model.*;
 
 public class Modeling {
 	private Model model;
 
-	public void addStatement(String s, String p, String o){
+	public void addStatement(String s, String p, String o) {
 		Resource subject = model.createResource(s);
 		Property predicate = model.createProperty(p);
 		RDFNode object = model.createResource(o);
@@ -13,36 +14,44 @@ public class Modeling {
 		model.add(stmt);
 	}
 
-	public void addLiteralStatement(String s, String p, Object o){
+	public void addLiteralStatement(String s, String p, Object o) {
 		Resource subject = model.createResource(s);
 		Property predicate = model.createProperty(p);
 		Statement stmt = model.createLiteralStatement(subject, predicate, o);
 		model.add(stmt);
 	}
-	 public void createModel(Course course){
-		 model = ModelFactory.createDefaultModel();
-		 String co= "http://www.coursera.org/course#";
-		 String cath= "http://www.coursera.org/cathegory#";
-		 String se= "http://www.coursera.org/session#";
-		 String ins= "http://www.coursera.org/instructor#";
-		 String uni= "http://www.coursera.org/university#";
-		// String nsRDFS= "http://www.w3.org/2000/01/rdf-schema#";
-		 addStatement(co, co+"has", se);
-		 addStatement(co, co+"taughtby", ins);
-		 addStatement(co, co+"belongsto", cath);
-		 addStatement(co, co+"taughtin", uni);
-		 addStatement(co+course.getShortname(), co+"has", se);
-		 addLiteralStatement(co+course.getShortname(), co+"name", course.getCourseName());
-		 addLiteralStatement(co+course.getShortname(), co+"id", course.getId());
-		 addLiteralStatement(co+course.getShortname(), co+"has", course.getId());
-	com.hp.hpl.jena.graph.Node blanknode= NodeFactory.createAnon();
-	
-		 
-		 
-		 
-	 }
 
-	public void writeModel(){
-		model.write(System.out,"TTL");
+	public void createModel(Course[] courses) {
+		model = ModelFactory.createDefaultModel();
+
+		Bag bag = model.createBag();
+
+		for (Course course : courses) {
+			String co = "http://www.coursera.org/course#";
+			String cath = "http://www.coursera.org/cathegory#";
+			String se = "http://www.coursera.org/session#";
+			String ins = "http://www.coursera.org/instructor#";
+			String uni = "http://www.coursera.org/university#";
+			// String nsRDFS= "http://www.w3.org/2000/01/rdf-schema#";
+			addStatement(co, co + "has", se);
+			addStatement(co, co + "taughtby", ins);
+			addStatement(co, co + "belongsto", cath);
+			addStatement(co, co + "taughtin", uni);
+			addStatement(co + course.getShortname(), co + "has", se);
+
+			addLiteralStatement(co + course.getShortname(), co + "name",
+					course.getCourseName());
+			addLiteralStatement(co + course.getShortname(), co + "id",
+					course.getId());
+			addLiteralStatement(co + course.getShortname(), co + "has",
+					course.getId());
+			
+			bag.add()
+		}
+
+	}
+
+	public void writeModel() {
+		model.write(System.out, "TTL");
 	}
 }
