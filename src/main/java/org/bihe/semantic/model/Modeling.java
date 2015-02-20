e133152153c977698c9f4374ab84c8758a2a6d42
Samim Pezeshki <p.samim@gmail.com>
2015-02-20 19:48:39 +0330

    Fix modelling

--- src/main/java/org/bihe/semantic/model/Modeling.java
+++ src/main/java/org/bihe/semantic/model/Modeling.java
@@ -1,10 +1,10 @@
 package org.bihe.semantic.model;
 
-import com.hp.hpl.jena.graph.NodeFactory;
 import com.hp.hpl.jena.rdf.model.*;
 
 public class Modeling {
 	private Model model;
+	private Bag bag;
 
 	public static void main(String[] args) {
 
@@ -16,7 +16,7 @@ public class Modeling {
 		Course course = new Course();
 		course.setCourseName("Test Course");
 		course.setId(1);
-		course.setShortname("Short test name");
+		course.setShortname("ShortTestName");
 
 		Course[] courses = { course };
 		Modeling modeling = new Modeling();
@@ -41,29 +41,25 @@ public class Modeling {
 		model.add(stmt);
 	}
 
-	Bag bag = model.createBag();
-
 	public void createModel(Course[] courses) {
 		model = ModelFactory.createDefaultModel();
+		bag = model.createBag();
+			// String nsRDFS= "http://www.w3.org/2000/01/rdf-schema#";
+//			addStatement(Constant.COURSE_URI, Constant.COURSE_URI + "has",
+//					Constant.SESSION_URI);
+//			addStatement(Constant.COURSE_URI, Constant.COURSE_URI + "taughtby",
+//					Constant.INSTRUCTOR_URI);
+//			addStatement(Constant.COURSE_URI,
+//					Constant.COURSE_URI + "belongsto", Constant.CATHEGORY_URI);
+//			addStatement(Constant.COURSE_URI, Constant.COURSE_URI + "taughtin",
+//					Constant.CATHEGORY_URI);
 
 		for (Course course : courses) {
-
-			// String nsRDFS= "http://www.w3.org/2000/01/rdf-schema#";
-			addStatement(Constant.COURSE_URI, Constant.COURSE_URI + "has",
-					Constant.SESSION_URI);
-			addStatement(Constant.COURSE_URI, Constant.COURSE_URI + "taughtby",
-					Constant.INSTRUCTOR_URI);
-			addStatement(Constant.COURSE_URI,
-					Constant.COURSE_URI + "belongsto", Constant.CATHEGORY_URI);
-			addStatement(Constant.COURSE_URI, Constant.COURSE_URI + "taughtin",
-					Constant.CATHEGORY_URI);
-
 			addLiteralStatement(Constant.COURSE_URI + course.getShortname(),
 					Constant.COURSE_URI + "name", course.getCourseName());
 			addLiteralStatement(Constant.COURSE_URI + course.getShortname(),
 					Constant.COURSE_URI + "id", course.getId());
-			addLiteralStatement(Constant.COURSE_URI + course.getShortname(),
-					Constant.COURSE_URI + "has", course.getId());
+
 			addToBag(Constant.COURSE_URI + course.getShortname());
 		}
 
