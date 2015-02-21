ce2b0f9a9e95863f915a7dccc4a4140fe2a517ce
rvm1380271 <rozita.raissi@gmail.com>
2015-02-21 12:26:45 +0330

    Setting shortName and name and url and universities of course

--- src/main/java/org/bihe/semantic/SPARQLParser/OpenUniversitySPARQLParser.java
+++ src/main/java/org/bihe/semantic/SPARQLParser/OpenUniversitySPARQLParser.java
@@ -1,6 +1,7 @@
 package org.bihe.semantic.SPARQLParser;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.bihe.semantic.model.Category;
@@ -16,12 +17,21 @@ import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.query.ResultSetFormatter;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Literal;
+import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.sparql.engine.http.QueryEngineHTTP;
 
+//Import log4j classes.
+import org.apache.log4j.Logger;
+import org.apache.log4j.LogManager;
+
 public class OpenUniversitySPARQLParser {
+	// Define a static logger variable so that it references the
+	// Logger instance named "MyApp".
+	private static final Logger logger = LogManager.getLogger(OpenUniversitySPARQLParser.class);
+	
 	static ArrayList<CourseInfo> courseLists = new ArrayList<CourseInfo>();
 	static ArrayList<Category> categoryLists = new ArrayList<Category>();
 	static ArrayList<Instructor> instructorLists = new ArrayList<Instructor>();
@@ -39,14 +49,19 @@ public class OpenUniversitySPARQLParser {
 	
 	public ArrayList<Course> getCoursesByName(String courseName)
 			throws Exception {
+		logger.trace("Entering application.");
+		
 		ArrayList<Course> courseDetailLists = new ArrayList<Course>();
 		
 		 String queryStr = 
 				 "PREFIX mlo: <http://purl.org/net/mlo/>" +
-	    			"SELECT DISTINCT ?title ?url " +
+	    			"SELECT DISTINCT ?url ?title ?label ?university ?id" +
 	    			"where { " +
 	    			"?thing <http://purl.org/dc/terms/title> ?title; " +
-	    			"<http://dbpedia.org/property/url> ?url. " +
+	    			"<http://www.w3.org/2000/01/rdf-schema#label> ?label;" +
+	    			"<http://purl.org/dc/elements/1.1/identifier> ?id;" +
+	    			"<http://courseware.rkbexplorer.com/ontologies/courseware#taught-at> ?university; " +
+	    			"<http://purl.org/net/mlo/url> ?url. " +
 	    			"FILTER regex(?title, '" + courseName + "', 'i' ) } \n ";
 		 Query query = QueryFactory.create(queryStr);
 		 // Remote execution.
@@ -54,16 +69,59 @@ public class OpenUniversitySPARQLParser {
 			 // Set the DBpedia specific timeout.
 			 ((QueryEngineHTTP)qexec).addParam("timeout", "10000") ;
 			 // Execute.
-			 ResultSet rs = qexec.execSelect();
-			 List<String> resultVars =  rs.getResultVars();
-			 for (int i = 0; i < resultVars.size(); i++) {
-				System.out.print(resultVars.get(i));
+			 ResultSet rs = qexec.execSelect() ;		 
+			 // The order of results is undefined.
+			 for ( ; rs.hasNext() ; )
+			 {
+				 QuerySolution rb = rs.nextSolution() ;
+				 Course crs = new Course();
+				 // Get title - variable names do not include the '?' (or '$')
+				 RDFNode x = rb.get("title") ;
+				 // Check the type of the result value
+				 if ( x.isLiteral() )
+				 {
+					 Literal titleStr = (Literal)x ;
+					 System.out.println(" "+titleStr.getValue().toString()) ;
+					 crs.setCourseName(titleStr.getValue().toString());
+				 }
+				 x = rb.get("label") ;
+				 // Check the type of the result value
+				 if ( x.isLiteral() )
+				 {
+					 Literal labelStr = (Literal)x ;
+					 System.out.println(" "+labelStr.getValue().toString()) ;
+					 crs.setShortname(labelStr.getValue().toString());
+				 }
+				 x = rb.get("url") ;
+				 // Check the type of the result value
+				 if ( x.isLiteral() )
+				 {
+					 Literal urlStr = (Literal)x ;
+					 System.out.println(" "+urlStr.getValue().toString()) ;
+					 ArrayList<Session> sessionsName = new ArrayList<Session>();
+					 Session session = new Session();
+					 session.setHomepage(urlStr.getValue().toString());
+					 sessionsName.add(session);
+					 crs.setSessions(sessionsName);
+				 }
+				 x = rb.get("university") ;
+				 // Check the type of the result value
+				 if ( x.isLiteral() )
+				 {
+					 Literal universityStr = (Literal)x ;
+					 System.out.println(" "+universityStr.getValue().toString()) ;
+					 ArrayList<University> universitiesName = new ArrayList<University>();
+					 University university = new University();
+					 university.setName(universityStr.getValue().toString());
+					 universitiesName.add(university);
+					 crs.setUniversities(universitiesName);
+				 }
+				 courseDetailLists.add(crs);
 			 }
-			 ResultSetFormatter.out(System.out, rs, query);
 		 } catch (Exception e) {
 			 e.printStackTrace();
 		 }
-	    
+		logger.trace("Exiting application.");
 		return courseDetailLists;
 	}
 }
