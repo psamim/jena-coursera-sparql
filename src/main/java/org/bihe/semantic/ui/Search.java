1428f32ca8c4e51e8a78f9114a3afba42c53db4a
Samim Pezeshki <p.samim@gmail.com>
2015-02-21 09:01:27 +0330

    Refactor Search

--- src/main/java/org/bihe/semantic/ui/Search.java
+++ src/main/java/org/bihe/semantic/ui/Search.java
@@ -1,11 +1,12 @@
 package org.bihe.semantic.ui;
 
 import java.util.ArrayList;
-
 import org.bihe.semantic.SPARQLParser.OpenUniversitySPARQLParser;
 import org.bihe.semantic.jsonParser.CourseraJSonParser;
 import org.bihe.semantic.model.Course;
+import org.bihe.semantic.model.Modeling;
 import org.bihe.semantic.utility.Utility;
+import com.hp.hpl.jena.rdf.model.Model;
 
 public class Search {
 	private String name;
@@ -29,23 +30,23 @@ public class Search {
 
 	public void getResults() {
 		// Pass the course that user wants to find it in Coursera
-
 		System.out.println(" Results on Coursera : ");
 		try {
 			CourseraJSonParser coursera = new CourseraJSonParser();
 			ArrayList<Course> courseraCourseDetails = coursera
 					.getCoursesByName(getName());
 			Utility.printList(courseraCourseDetails);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println(" Results on Open University : ");
-		try {
-			System.out.println(getName());
+
+			System.out.println(" Results on Open University : ");
 			OpenUniversitySPARQLParser ou = new OpenUniversitySPARQLParser();
-			ArrayList<Course> courseraCourseDetails = ou
-					.getCoursesByName(getName());
-			Utility.printList(courseraCourseDetails);
+			ArrayList<Course> ouCourseDetails = ou.getCoursesByName(getName());
+			Utility.printList(ouCourseDetails);
+			
+			@SuppressWarnings("unchecked")
+			ArrayList<Course> courses = (ArrayList<Course>) courseraCourseDetails.clone();
+			courses.addAll(ouCourseDetails);
+			Model model = new Modeling().createModel(courses);
+			model.write(System.out, "TTL");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
